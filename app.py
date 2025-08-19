c0ee14bb07bf2720c2677a30a032ca7349cf0ca3
stackoverflowed9 <arjuncoder27@gmail.com>
2025-08-20 01:01:42 +0530

    birthday Website completed

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/message")
+def message():
+    return render_template("message.html")
+
+
+@app.route("/collage")
+def collage():
+    return render_template("collage.html")
+
+
+if __name__ == "__main__":
+    app.run()
